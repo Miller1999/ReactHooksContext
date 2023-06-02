35c0f72084fdd17f1adc67297bd06fd4a57d2d11
Miller1999 <millerjavierarias@gmail.com>
2023-06-02 00:10:05 -0500

    Terminado el 3 modulo

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
@@ -6,9 +6,18 @@ import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
+import Step from "./Step";
 
+//validaciones
+
+import { validarEmail,validarPassword } from "./DatosUsuario/validaciones";
 const Form = () => {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(0);
+  const [pasos,setPasos] = useState({})
+
+  useEffect(()=>{
+
+  })
 
   //step = 0 --> <DatosUsuario />
   //step = 1 --> <DatosPersonales />
@@ -27,6 +36,42 @@ const Form = () => {
     3: <Complete />,
   };
 
+  const onSubmit = () => {
+    let newStep = step +1;
+    setStep(newStep)
+  }
+
+  const handleChange = (element,position,currentStep,validator) => {
+    const value = element.target.value
+    const valid = validator(value)
+  }
+  
+  const stepsFlow = {
+    0: {
+      inputs:[
+        {
+          label: "Correo Electronico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa un correo electronico válido",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20.",
+          validator: validarPassword
+        }
+      ],
+      buttonText: "Siguiente",
+      onSubmit
+    }
+  }
   return (
     <Box
       sx={{
@@ -41,10 +86,8 @@ const Form = () => {
       </LogoSpace>
       <FormSpace>
         {step < 3 && <Stepper step={step} />}
-        {/* <DatosUsuario />
-        <DatosPersonales />
-        <DatosEntrega /> */}
-        {steps[step]}
+        {/*steps[step]*/}
+        <Step data={stepsFlow[step]} step={step}/>
       </FormSpace>
     </Box>
   );
